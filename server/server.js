18bbc9bf7c83db05401189c29bcdafe6ccbaa8da
Rajneesh Yadav <yadavrajneesh481@gmail.com>
2025-12-12 16:12:40 +0530

    add till react

--- server/server.js
+++ server/server.js
@@ -2,28 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
-const path = require("path");
-dotenv.config();
 
+dotenv.config();
 const app = express();
 
+// CORS
 app.use(cors({
-  origin: 'https://codedecoder-git-main-yadavrajneesh481s-projects.vercel.app/' // or '*' for dev
+  origin: "https://codedecoder-git-main-yadavrajneesh481s-projects.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
+
 app.use(express.json());
 
+// DB
 connectDB();
 
 // API routes
 app.use("/api/auth", require("./routes/authRoutes.js"));
 app.use("/api/explain", require("./routes/explainRoutes.js"));
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-app.listen(process.env.PORT, () => {
+// Start
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
